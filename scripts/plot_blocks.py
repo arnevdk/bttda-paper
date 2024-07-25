9538a4347110faf740e961bfbdf7c8af7f5c91d9
Arne Van Den Kerchove <arne@vandenkerchove.com>
2024-07-25 17:01:49 +0200

    New block figure

--- scripts/plot_blocks.py
+++ scripts/plot_blocks.py
@@ -8,29 +8,57 @@ import seaborn as sns
 
 from setup import set_size, textwidth
 
-df = pd.read_csv("data/erp_blocks.csv")
+df_select = pd.read_csv("data/erp_blocks_select.csv")
+df_select["dataset"] = "BNCI2014-008"
 
-fig, axs = plt.subplots(2, 3, layout="tight", sharex=True)
-lineplot_kwargs = dict(
-    data=df,
-    x="block",
-    hue="dataset",
-    # style="session",
-    errorbar=None,
-    # units="subject",
-    # estimator=None,
+df_train = pd.read_csv("data/erp_blocks_train.csv")
+df_train = df_train.reset_index()
+df_train["dataset"] = "BNCI2014-008"
+
+idx = ["dataset", "subject", "session", "select", "fold"]
+df_score_train = df_select.groupby(idx)["train_score"].aggregate("first")
+df_score_val = df_select.groupby(idx)["val_score"].aggregate("first")
+df_score_test = df_select.groupby(idx)["test_score"].aggregate("first")
+df_diff = df_select.set_index(idx + ["block"])
+df_diff["train"] = df_diff["train_score"] - df_score_train
+df_diff["validation"] = df_diff["val_score"] - df_score_val
+df_diff["test"] = df_diff["test_score"] - df_score_test
+df_diff = df_diff.reset_index()
+
+df_diff = df_diff.melt(
+    id_vars=idx + ["block"],
+    value_vars=["train", "validation", "test"],
+    var_name="split",
+    value_name="roc_auc_diff",
 )
-sns.lineplot(y="F_rt", ax=axs[0, 0], legend=True, **lineplot_kwargs)
-axs[0, 0].set_ylabel("$\phi$")
-axs[0, 0].ticklabel_format(axis="y", style="sci", scilimits=(0, 0))
-sns.lineplot(y="mse", ax=axs[0, 1], legend=False, **lineplot_kwargs)
-axs[0, 1].set_ylabel("MSE")
-sns.lineplot(y="val_score", ax=axs[0, 2], legend=False, **lineplot_kwargs)
-axs[0, 2].set_ylabel("ROC-AUC")
+
+df_diff["n_blocks"] = df_diff["block"] + 1
+df_diff["roc_auc_diff_%"] = df_diff["roc_auc_diff"] * 100
+fig, axs = plt.subplots(1, 2, layout="tight", sharex=True)
+lineplot_kwargs = dict(
+    x="n_blocks",
+    hue="dataset",
+    errorbar=None,
+)
+
+
+df_train["n_blocks"] = df_train["block"]
+
+sns.lineplot(data=df_train, y="nmse", ax=axs[0], legend=False, **lineplot_kwargs)
+axs[0].set_ylabel("NMSE")
+sns.lineplot(
+    data=df_diff,
+    y="roc_auc_diff_%",
+    style="split",
+    ax=axs[1],
+    legend=True,
+    **lineplot_kwargs
+)
+axs[1].set_ylabel("$\Delta$ROC-AUC (%.)")
 for ax in axs.flatten():
     ax.set_xlabel("$b$")
-    ax.set_xlim([1, 32])
-fig.set_size_inches(set_size(width_pt=textwidth, subplots=(2, 3)))
+    ax.set_xlim([1, 10])
+fig.set_size_inches(set_size(width_pt=textwidth, subplots=(1, 2)))
 fig.tight_layout()
 fig.savefig("figures/blocks.pgf", bbox_inches="tight", pad_inches=0)
 fig.savefig("figures/blocks.svg", bbox_inches="tight", pad_inches=0)
