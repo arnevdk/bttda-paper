07b26a3154ed70adf20ceef9f5d6fc44ee1dd10a
Arne Van Den Kerchove <arne@vandenkerchove.com>
2025-10-28 18:08:13 +0100

    Add interpretability figure

---
+++ scripts/interpretability_erp.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+import mne
+import pandas as pd
+from mne.viz import plot_topomap
+
+contrast_grand_avg = mne.read_evokeds(
+    "data/interpretability/erp_contrast_grand-avg_ave.fif", verbose=False
+)[0]
+contrast_b1 = mne.read_evokeds(
+    "data/interpretability/erp_contrast_block-1_ave.fif", verbose=False
+)[0]
+contrast_b2 = mne.read_evokeds(
+    "data/interpretability/erp_contrast_block-2_ave.fif", verbose=False
+)[0]
+
+
+def save_evoked(evoked, name):
+    df = pd.DataFrame(evoked.data.T * 1e6, index=evoked.times, columns=evoked.ch_names)
+    df.index.name = "time"
+    df.to_csv(f"data/interpretability/erp_{name}.csv")
+
+
+save_evoked(contrast_grand_avg, "contrast_grand-avg")
+save_evoked(contrast_b1, "contrast_block-1")
+save_evoked(contrast_b2, "contrast_block-2")
+
+
+def save_topomap(evoked, name, t=0):
+    s = int((t - evoked.tmin) * evoked.info["sfreq"])
+    topo = evoked.data[:, s]
+    plot_topomap(topo, evoked.info, show=False)
+    plt.savefig(
+        f"figures/interpretability/erp_{name}_topo.png",
+        bbox_inches="tight",
+        pad_inches=0,
+        transparent=True,
+        dpi=1000,
+    )
+
+
+save_topomap(contrast_grand_avg, "contrast_grand-avg", t=0.45)
+save_topomap(contrast_b1, "contrast_block-1", t=0.30)
+save_topomap(contrast_b2, "contrast_block-2", t=0.30)
---
+++ scripts/interpretability_mi.py
@@ -0,0 +1,62 @@
+import matplotlib.pyplot as plt
+import mne
+import numpy as np
+import pandas as pd
+from matplotlib.transforms import Bbox
+from mne.time_frequency import read_tfrs
+from mne.viz import plot_topomap
+
+axis_width = 0.25
+plt.rcParams.update(
+    {
+        "text.usetex": True,
+        "axes.linewidth": axis_width,
+        "ytick.major.width": axis_width,
+    }
+)
+
+contrast_grand_avg = read_tfrs(
+    "data/interpretability/mi_contrast_grand-avg_tfr.h5", verbose=False
+)
+contrast_b1 = read_tfrs(
+    "data/interpretability/mi_contrast_block-1_tfr.h5", verbose=False
+)
+contrast_b2 = read_tfrs(
+    "data/interpretability/mi_contrast_block-2_tfr.h5", verbose=False
+)
+
+
+roi = [
+    dict(chan="C3", tmin=0.3, tmax=3, fmin=9, fmax=14),
+    dict(chan="C3", tmin=1.3, tmax=2, fmin=8, fmax=12),
+]
+
+
+def viz_tfr(evoked, name, chan="C3", t=1, f=10):
+    figs = evoked.copy().pick(chan).plot(show=False, colorbar=False)
+    figs[0].gca().set_axis_off()
+    plt.savefig(
+        f"figures/interpretability/mi_contrast_{name}_spec.png",
+        bbox_inches="tight",
+        pad_inches=0,
+        dpi=1000,
+        transparent=True,
+    )
+
+    sfreq = len(evoked.times) / np.max(evoked.times)
+    s = int((t - evoked.tmin) * sfreq)
+    fs = np.where(evoked.freqs > f)[0][0]
+    topo = evoked.data[:, fs, s]
+    plot_topomap(topo, evoked.info, show=False)
+    plt.savefig(
+        f"figures/interpretability/mi_contrast_{name}_topo.png",
+        bbox_inches="tight",
+        pad_inches=0,
+        dpi=1000,
+        transparent=True,
+    )
+
+
+viz_tfr(contrast_grand_avg, "grand-avg", chan="C3", t=1, f=11)
+viz_tfr(contrast_b1, "block-1", chan="C3", t=1, f=11)
+viz_tfr(contrast_b2, "block-2", chan="C3", t=1.6, f=10)
